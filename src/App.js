d24c8669f818aaf5e25102c53b7887aebda4bd23
Sibylle Sehl <sibylle.sehl@googlemail.com>
2019-02-08 13:24:10 +0100

    [TASK] add info to repo

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ const App = () => {
   return (
     <ListWrapper>
       <h1> This list is gonna be made with awesome hooks!</h1>
+      <p>This example uses the useState() hook</p>
       <ul>
         {list.map(item => (
           <li key={item.id}>
